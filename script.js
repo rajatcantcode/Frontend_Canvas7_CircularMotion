4002eed38fd2566e8d93a0fb787c5c6b402bfd06
Rajat <130392640+rajatcantcode@users.noreply.github.com>
2024-02-07 16:05:36 +0530

    moving one particle in circular motion completed

--- script.js
+++ script.js
@@ -14,26 +14,15 @@ const mouse = {
 
 const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
 
-// Event Listeners
-addEventListener("mousemove", (event) => {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-
-addEventListener("resize", () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-
-  init();
-});
-
 // Objects
-class Object {
+class Particle {
   constructor(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.radians = 0;
+    this.velocity = 0.05;
   }
 
   draw() {
@@ -45,17 +34,24 @@ class Object {
   }
 
   update() {
+    this.radians += this.velocity;
+    this.x = this.x + Math.cos(this.radians) * 10;
+
+    this.y = this.y + Math.sin(this.radians) * 10;
+
     this.draw();
   }
 }
 
 // Implementation
-let objects;
+let particles;
 function init() {
-  objects = [];
+  particles = [];
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+  for (let i = 0; i < 1; i++) {
+    particles.push(
+      new Particle(canvas.width / 2, canvas.height / 2, 10, "blue")
+    );
   }
 }
 
@@ -64,11 +60,23 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
-  c.fillText("HTML CANVAS BOILERPLATE", mouse.x, mouse.y);
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  particles.forEach((particle) => {
+    particle.update();
+  });
 }
 
 init();
 animate();
+
+// Event Listeners
+addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
+//Resize
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init();
+});
